3a5e02a8ba3c3799d9d6969bc5136824bc90d412
Kirill <dissin101@gmail.com>
2020-04-10 22:20:41 +0600

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -17,6 +18,17 @@ app.use("/auth", require("./routes/auth"));
 app.use("/user", require("./routes/users"));
 app.use("/", require("./routes/publication"));
 
-const PORT = process.env.PORT || 3001;
+// Serve static assets in production
+
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`[server]  started on port ${PORT}`));
