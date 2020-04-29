73dd8dbfc051172fb6ec5bfc55d04ee9caf901a7
Kirill <dissin101@gmail.com>
2020-04-29 15:01:33 +0600

    prepare to deploy on heroku

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,8 +1,8 @@
-import Login from "./Login";
-import Register from "./Register";
+import Login from "./login";
+import Register from "./register";
 import Publications from "./publications";
 import ProductPage from "./productPage";
 import Profile from "./profile";
 import AddPublication from "./add-publication";
-//
+
 export { Login, Register, Publications, ProductPage, Profile, AddPublication };
