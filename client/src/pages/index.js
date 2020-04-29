a7cc85859df30ac4c678ee5d3b253ffdc58e31f1
Kirill <dissin101@gmail.com>
2020-04-29 15:43:35 +0600

    fix: routes

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,5 @@
-import Login from "./login";
-import Register from "./register";
+import Login from "./Login";
+import Register from "./Register";
 import Publications from "./publications";
 import ProductPage from "./productPage";
 import Profile from "./profile";
