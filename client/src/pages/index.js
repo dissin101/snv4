71fa2f27ab74f16871ccd14d23f8e5cf5478ffe3
Kirill <dissin101@gmail.com>
2020-04-29 15:06:54 +0600

    prepare to deploy

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,5 @@
 import Login from "./login";
-import Register from "./register";
+import Register from "./Register";
 import Publications from "./publications";
 import ProductPage from "./productPage";
 import Profile from "./profile";
